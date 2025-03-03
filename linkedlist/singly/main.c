f21b4f5e8fe4c61a1886faef0d204b14c43653aa
balbir <prasadbalbir1056@gmail.com>
2025-03-03 20:02:55 +0530

    singly: main file

--- linkedlist/singly/main.c
+++ linkedlist/singly/main.c
@@ -13,20 +13,32 @@ int main(int argc, char **argv)
   // if(p->next == NULL || p->next==0 || !p-next)
   int arr[] = {2, 4, 6, 8, 10};
   struct Node *head = NULL;
-  struct Node *tail = NULL;
   for (int i = 0; i < 5; i++)
   {
     insertAtEnd(&head, arr[i]);
   }
+  int arr2[] = {1, 3, 4, 5, 9, 12};
+  struct Node *head2 = NULL;
+  for (int i = 0; i < 6; i++)
+  {
+    insertAtEnd(&head2, arr2[i]);
+  }
+  // display(head);
+  // puts("\n");
+  // deleteFirst(&head);
+  // deleteLast(&head);
+  // display(head);
+  // puts("\n");
+  // insertAtBeginning(&head, 5);
+  // display(head);
+  // puts("\n");
+  // reverseList(&head);
   display(head);
   puts("\n");
-  deleteFirst(&head);
-  deleteLast(&head);
-  display(head);
+  display(head2);
   puts("\n");
-  insertAtBeginning(&head, 5);
-  display(head);
-  puts("\n");
+  struct Node *ans = mergeSortedList(head, head2);
+  display(ans);
 
   return 0;
 }
