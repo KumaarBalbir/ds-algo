929e3dc976e0aafd851329dd3a52d4110deff428
balbir <prasadbalbir1056@gmail.com>
2025-03-25 20:44:29 +0530

    adds q defn

---
+++ qu/using-arr/qu.h
@@ -0,0 +1,20 @@
+#ifndef QU_H
+#def QU_H
+
+#include<iostream>
+
+struct Queue
+{
+  int front, rear, capacity;
+  int *queue;
+  Queue(int c);
+  ~Queue();
+  void Enqueue(int val);
+  int Dequeue();
+  void display();
+  void queueFront();
+
+};
+
+
+#endif 
