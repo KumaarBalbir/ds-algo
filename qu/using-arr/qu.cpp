2a53308c5e02e41e68e950ec95b4a3837460e95c
balbir <prasadbalbir1056@gmail.com>
2025-03-28 23:03:30 +0530

    bug fixes: wrong condition of full and empty

--- qu/using-arr/qu.cpp
+++ qu/using-arr/qu.cpp
@@ -2,10 +2,9 @@
 
 Queue::Queue(int c)
 {
-  front = rear = 0;
+  front = rear = -1;
   capacity = c;
   queue = new int[c];
-
 }
 
 Queue::~Queue()
@@ -15,47 +14,56 @@ Queue::~Queue()
 
 void Queue::Enqueue(int val)
 {
-  if((rear+1)%capacity == front)
-    {
-      printf("Q is full\n");
-      
-
-    }else
-    {
-      queue[rear] = val;
-      rear = (rear+1)%capacity;
-    }
+  if ((rear + 1) % capacity == front)
+  {
+    printf("Q is full\n");
+    return;
+  }
+  if (front == -1) // first element
+  {
+    front = 0;
+  }
+  rear = (rear + 1) % capacity;
+  queue[rear] = val;
 }
 
 int Queue::Dequeue()
 {
-  if(front == rear)
-    {
-      printf("Q is empty\n");
-      return -1;
-    }
-  int data = queue[front];
-  front = (front+1)%capacity;
-  
+  int data;
+  if (front == -1)
+  {
+    printf("Q is empty\n");
+    return -1;
+  }
+  if (front == rear) // last element
+  {
+    data = queue[front];
+    front = rear = -1;
+  }
+  else
+  {
+    data = queue[front];
+    front = (front + 1) % capacity;
+  }
   return data;
 }
 
 void Queue::display()
 {
-  if(front == rear)
+  if (front == -1)
+  {
+    printf("Q is empty\n");
+    return;
+  }
+  int i = front;
+  while (true)
+  {
+    printf("%d ", queue[i]);
+    if (i == rear)
     {
-      printf("Q is empty\n");
-    }else
-    {
-      for(int i=front;i<rear;i++)
-	{
-	  if(i!=rear-1)
-	  printf("%d <-- ",queue[i]);
-	  else
-	    printf("%d",queue[i]);
-	}
+      break;
     }
+    i = (i + 1) % capacity;
+  }
+  printf("\n");
 }
-
-	  
-  
