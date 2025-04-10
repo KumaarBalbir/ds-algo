c0987edaed6454059dc14a48286caba4cb1dfd64
balbir <prasadbalbir1056@gmail.com>
2025-04-10 22:26:34 +0530

    dqu using ll: implements insertFront

--- dqu/using-ll/dqu.cpp
+++ dqu/using-ll/dqu.cpp
@@ -22,3 +22,18 @@ DEQueue::~DEQueue()
     delete temp;
   }
 }
+
+void DEQueue::insertFront(int val)
+{
+  Node *newNode = new Node(val);
+  if (front == nullptr)
+  {
+    front = rear = newNode;
+  }
+  else
+  {
+    newNode->next = front;
+    front->prev = newNode;
+    front = newNode;
+  }
+}
