e056bb5876b8809bd8fccc3904204b70137ac0ed
balbir <prasadbalbir1056@gmail.com>
2025-04-11 07:57:25 +0530

    2-stk-qu: implements dequeue

--- qu/2-stk-qu/qu.cpp
+++ qu/2-stk-qu/qu.cpp
@@ -9,3 +9,21 @@ bool Queue::isEmpty()
 {
   return e_st.empty() && d_st.empty();
 }
+int Queue::dequeue()
+{
+  if (d_st.empty())
+  {
+    while (!e_st.empty())
+    {
+      d_st.push(e_st.top());
+      e_st.pop();
+    }
+  }
+  if (d_st.empty())
+  {
+    return -1; // Queue is empty
+  }
+  int x = d_st.top();
+  d_st.pop();
+  return x;
+}
